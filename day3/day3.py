01859341c17c34c862ea51d3187096aef0cacda7
Jonathan Dang <jdang@wpi.edu>
2019-12-11 10:32:43 -0500

    completed day 3 part two

--- day3/day3.py
+++ day3/day3.py
@@ -19,13 +19,21 @@ def containsKey(key, val) :
 def getDistanceFromOrigin(originX, originY, currX, currY) :
     return abs(originX - currX) + abs(originY - currY)
 
+
+# returns the number of steps it took wire 1 to get to
+def getSteps(posX, posY) :
+    global masterMap
+
+    return masterMap[posX][posY]
+
 # inputs is a single wire
-def move(wire, line, minDistance) :
+def move(wire, line, minDistance, minSteps) :
     global masterMap, originX, originY
 
     currX = originX 
     currY = originY
-
+    counter = 0
+    
     for mov in wire :
         direction = mov[:1]
         steps = int(mov[1:])
@@ -51,37 +59,41 @@ def move(wire, line, minDistance) :
         for _ in range(steps) :
             currX += dirX 
             currY += dirY 
-
+            counter += 1
             if line == 2 and containsKey(currX, currY) is True :
   
                 dist = getDistanceFromOrigin(originX, originY, currX, currY)
-
                 minDistance = min(minDistance, dist)
+                calSteps = counter + getSteps(currX, currY)
+                minSteps = min(minSteps, calSteps)
 
             if line == 1:
                 # using nested map
                 if currX in masterMap :
-                    masterMap[currX][currY] = currX
+                    masterMap[currX][currY] = counter
 
                 else :
                     masterMap[currX] = {}
-                    masterMap[currX][currY] = currX
+                    masterMap[currX][currY] = counter 
 
-    return minDistance
+    return minDistance, minSteps
+                    
 
-def partOne(inputs) :
+def Parts_One_and_Two(inputs) :
     global masterMap
     wire1 = inputs[0].split(',')
     wire2 = inputs[1].split(',')
     minDistance = sys.maxsize
+    minSteps = sys.maxsize
 
-    minDistance = move(wire1, 1, minDistance)
-    minDistance = move(wire2, 2, minDistance)
+    minDistance, minSteps = move(wire1, 1, minDistance, minSteps)
+    minDistance, minSteps = move(wire2, 2, minDistance, minSteps)
 
-    print('\nMinimum Distance From Origin: ', minDistance, '\n')
+    print('\nMinimum Distance From Origin: ', minDistance)
+    print('Minimum Number of Steps to Intersection: ', minSteps, '\n')
 
 if __name__ == '__main__' :
     with open('input.txt') as file :
         inputs = file.read().splitlines()
 
-    partOne(inputs)
+    Parts_One_and_Two(inputs)
